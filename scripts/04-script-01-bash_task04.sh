91317a3e63986f5390c3afd192041d6b9c3ce792
notfounder <vnenaydenko@gmail.com>
2023-04-16 19:22:04 +0300

    Done homework 04-script-01-bash

---
+++ scripts/04-script-01-bash_task04.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+##############################
+#Project: Homework Netology
+#Version 1.0
+#(c) 2023 Notfounder
+##############################
+
+ip_address=("192.168.0.1" "173.194.222.113" "87.250.250.242")
+port=80
+
+for ip_check in ${ip_address[@]}; do #перебираем массив ip адресов
+
+    for ((count = 1; count < 6; count++)); do #перебираем каждое значение из массива 5 раз
+        date=$(date "+%d.%m.%Y %H:%M:%S")
+        nc -w 1 -z $ip_check $port 2> /dev/null #проверяем доступность IP адреса по порту
+        
+        if (($? == 0)); then
+            
+            result="$date available IP:$ip_check:$port"
+
+        else
+            
+            result="$date NOT AVAILABLE IP:$ip_check:$port"
+            echo $result >>available_error.log #записываем в error log результат
+            exit 0 #завершаем работу скрипта
+        fi
+        
+        echo $result >>available.log #записываем результат
+    
+    done
+
+done
